71ec9caffa57a82ed9e037c90a91cccf07aa99f3
MarcoLvr <marco.lvr@outlook.it>
2023-03-22 10:31:39 +0100

    Changes by 19479

---
+++ server/src/main/java/me/marcolvr/server/game/logic/BlackJackDealer.java
@@ -0,0 +1,17 @@
+package me.marcolvr.server.game.logic;
+
+import me.marcolvr.server.game.logic.card.BlackJackCard;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class BlackJackDealer {
+    private List<BlackJackCard> carte;
+
+    public BlackJackDealer() {
+        carte=new ArrayList<>();
+    }
+
+
+}
